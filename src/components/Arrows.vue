6a8e689d9008648d2b48f1077aab2b34f4a93308
刘昭廷 <liuzhaoting@100tal.com>
2017-10-13 11:33:08 +0800

    component add

---
+++ src/components/Arrows.vue
@@ -0,0 +1,79 @@
+<template>
+  <div ref="arrows"  class='arrows' :style="getDx">
+
+    <img class='arrow1'  src="../../static/img/arrow_graphic.png"/>
+    <img class='arrow2'  src="../../static/img/arrow_graphic.png"/>
+    <img class='arrow3'  src="../../static/img/arrow_graphic.png"/>
+
+  </div>
+</template>
+<script>
+  export default{
+    props:{
+      pdl:{
+        type:Number,
+        default:0
+      },
+      pdr:{
+        type:Number,
+        default:0
+      }
+    },
+    computed:{
+      getDx:function(){
+        const self = this;
+        return {
+          paddingLeft:self.dx+'rem',
+          paddingRight:self.dx+'rem',
+
+        }
+      }
+    },
+    data:function(){
+      return {
+
+      }
+    },
+    mounted:function(){
+
+    }
+  }
+</script>
+<style scoped="">
+img{
+  display: block;
+  position: relative;
+  width:0.72rem;
+  height:auto;
+}
+.arrows{
+  position: absolute;
+  margin:0 auto;
+  left:0px;
+  right:0px;
+  width:0.72rem;
+  top:9.7rem;
+  opacity: 1;
+}
+.arrow1{
+  animation:myanime1 0.8s linear 0s infinite alternate;
+-webkit-animation:myanime1 0.8s linear 0s infinite alternate;
+}
+.arrow2{
+  animation:myanime1 0.8s linear 0.3s infinite alternate;
+  -webkit-animation:myanime1 0.8s linear 0.3s infinite alternate;
+}
+.arrow3{
+  animation:myanime1 0.8s linear 0.6s infinite alternate;
+  -webkit-animation:myanime1 0.8s linear 0.6s infinite alternate;
+}
+@keyframes myanime1 {
+0%{
+  opacity: 1
+}
+100%{
+  opacity: 0.2
+}
+
+}
+</style>
---
+++ src/components/RadarGraphic.vue
@@ -0,0 +1,325 @@
+<template>
+
+
+    <svg ref="mysvg"  viewBox="0,0,1920,1080" width="100%" height="100%"  version="1.1"
+  xmlns="http://www.w3.org/2000/svg">
+      <g>
+
+        <polygon  :points="getgraphics"
+        style="fill:rgb(249,215,170);
+        stroke:#000000;stroke-width:0"/>
+      </g>
+
+      <g>
+        <polygon :points="texturegraphicsA"
+        style="fill:rgb(255,231,195);
+        stroke:#000000;stroke-width:0"/>
+      </g>
+
+      <g>
+        <polygon :points="texturegraphicsB"
+        style="fill:rgb(249,215,170);
+        stroke:#000000;stroke-width:0"/>
+      </g>
+
+      <g>
+        <polygon :points="texturegraphicsC"
+        style="fill:rgb(255,231,195);
+        stroke:#000000;stroke-width:0"/>
+      </g>
+
+      <g>
+        <polygon :points="texturegraphicsD"
+        style="fill:rgb(249,215,170);
+        stroke:#000000;stroke-width:0"/>
+      </g>
+
+
+
+      <g>
+        <polygon ref="innerGraphic" :points="innerGetgraphics"
+        style="fill:rgba(24,222,255,0.5);
+        stroke:#18a9ff;stroke-width:1"/>
+      </g>
+      <circle v-for="(item,index) in circlelist" :key="index" :cx="item.cx" :cy="item.cy" r="3"
+       fill="#18a9ff"/>
+       <text  v-for="(item,index) in svgText" :x="item.x" :y="item.y"  fill="#9a592a" style="font-size:0.4rem;font-weight:bold;" v-html="item.text"></text>
+  </svg>
+
+</template>
+<script>
+
+  export default{
+    props:{
+      pointCenter:{
+        type:Object,
+        default:function(){
+
+          return{
+            x:635,
+            y:440
+          }
+        }
+      },
+      scale:{
+        type:Number,
+        default:0.4
+      },
+
+      bianOuter:{
+        type:Number,
+        default:6
+      },
+      radiusOuter:{
+        type:Number,
+        default:165
+      },
+      bianInner:{
+        type:Number,
+        default:6
+      },
+      radiusInner:{
+        type:Number,
+        default:165
+      },
+      //初始化百分比分数布局点数组
+      point_Array:{
+        type:Array,
+        default:function(){
+          return [[160,200],[100,200],[100,200],[100,200],[100,200],[100,200]]
+        }
+      },
+
+    },
+    data:function(){
+      return{
+        circle1_x:0,
+        circle1_y:100,
+        circlelist:[{
+          cx:0,
+          cy:0
+        }],
+        svgText:[{
+          text:'e444'
+        }]
+
+      }
+    },
+    computed:{
+
+
+      texturegraphicsA:function(){
+        let ln = this.radiusOuter*0.8
+        let center_pt = {
+          x:this.pointCenter.x,
+          y:this.pointCenter.y
+        }
+        let bian = this.bianOuter;
+        let _angle = 360/bian;
+        let graphics_arr = [];
+
+        for(let i=1;i<=bian;i++){
+            let _bianAngle = _angle*i;
+            let pt = {
+              x:center_pt.x+Math.sin(_bianAngle*Math.PI/180)*ln,
+              y:center_pt.y+Math.cos(_bianAngle*Math.PI/180)*ln
+            }
+            let format = pt.x+','+pt.y
+            graphics_arr.push(format);
+        }
+        return graphics_arr.join(' ')
+      },
+      texturegraphicsB:function(){
+        let ln = this.radiusOuter*0.6
+        let center_pt = {
+          x:this.pointCenter.x,
+          y:this.pointCenter.y
+        }
+        let bian = this.bianOuter;
+        let _angle = 360/bian;
+        let graphics_arr = [];
+
+        for(let i=1;i<=bian;i++){
+            let _bianAngle = _angle*i;
+            let pt = {
+              x:center_pt.x+Math.sin(_bianAngle*Math.PI/180)*ln,
+              y:center_pt.y+Math.cos(_bianAngle*Math.PI/180)*ln
+            }
+            let format = pt.x+','+pt.y
+            graphics_arr.push(format);
+        }
+        return graphics_arr.join(' ')
+      },
+      texturegraphicsC:function(){
+        let ln = this.radiusOuter*0.4
+        let center_pt = {
+          x:this.pointCenter.x,
+          y:this.pointCenter.y
+        }
+        let bian = this.bianOuter;
+        let _angle = 360/bian;
+        let graphics_arr = [];
+
+        for(let i=1;i<=bian;i++){
+            let _bianAngle = _angle*i;
+            let pt = {
+              x:center_pt.x+Math.sin(_bianAngle*Math.PI/180)*ln,
+              y:center_pt.y+Math.cos(_bianAngle*Math.PI/180)*ln
+            }
+            let format = pt.x+','+pt.y
+            graphics_arr.push(format);
+        }
+        return graphics_arr.join(' ')
+      },
+      texturegraphicsD:function(){
+        let ln = this.radiusOuter*0.2
+        let center_pt = {
+          x:this.pointCenter.x,
+          y:this.pointCenter.y
+        }
+        let bian = this.bianOuter;
+        let _angle = 360/bian;
+        let graphics_arr = [];
+
+        for(let i=1;i<=bian;i++){
+            let _bianAngle = _angle*i;
+            let pt = {
+              x:center_pt.x+Math.sin(_bianAngle*Math.PI/180)*ln,
+              y:center_pt.y+Math.cos(_bianAngle*Math.PI/180)*ln
+            }
+            let format = pt.x+','+pt.y
+            graphics_arr.push(format);
+        }
+        return graphics_arr.join(' ')
+      },
+      //雷达图外部多边形点数组
+      getgraphics:function(){
+        const self = this;
+        let ln = this.radiusOuter
+        let center_pt = {
+          x:this.pointCenter.x,
+          y:this.pointCenter.y
+        }
+        let bian = this.bianOuter;
+        let _angle = 360/bian;
+        let graphics_arr = [];
+
+        for(let i=1;i<=bian;i++){
+            let _bianAngle = _angle*i;
+            let pt = {
+              x:center_pt.x+Math.sin(_bianAngle*Math.PI/180)*ln,
+              y:center_pt.y+Math.cos(_bianAngle*Math.PI/180)*ln
+            }
+            let format = pt.x+','+pt.y;
+
+            switch (i) {
+              case 1:
+              self.svgText.push({
+                x:pt.x+10,
+                y:pt.y,
+                text:'观察能力'
+              })
+                break;
+                case 2:
+                self.svgText.push({
+                  x:pt.x+10,
+                  y:pt.y,
+                  text:'推理能力'
+                })
+                  break;
+                  case 3:
+                  self.svgText.push({
+                    x:pt.x-40,
+                    y:pt.y-18,
+                    text:'专注力'
+                  })
+                    break;
+                    case 4:
+                    self.svgText.push({
+                      x:pt.x-105,
+                      y:pt.y,
+                      text:'运算能力'
+                    })
+                      break;
+                      case 5:
+                      self.svgText.push({
+                        x:pt.x-105,
+                        y:pt.y,
+                        text:'时间管理'
+                      })
+                        break;
+                        case 6:
+                        self.svgText.push({
+                          x:pt.x-48,
+                          y:pt.y+25,
+                          text:'动手能力'
+                        })
+                          break;
+              default:
+
+            }
+
+            graphics_arr.push(format);
+        }
+        return graphics_arr.join(' ')
+      },
+      //雷达图的内部多边形点数组
+      innerGetgraphics:function(){
+        const self = this;
+
+        let ln = this.radiusInner
+        let center_pt = {
+          x:this.pointCenter.x,
+          y:this.pointCenter.y
+        }
+        let bian = this.bianInner;
+        let _angle = 360/bian;
+
+      //计算出各个点的百分比
+      let percent_bian = [];
+      for(let i=0;i<this.point_Array.length;i++){
+          let percent = this.point_Array[i][0]/this.point_Array[i][1]
+          percent_bian.push(percent)
+      }
+       //svg多边形绘图点数组
+        let graphics_arr = [];
+        for(let i=1;i<=bian;i++){
+            let _bianchang = percent_bian[i-1]*ln;//得出每个百分比的数值
+            let _bianAngle = _angle*i;//得出每个边对应角度
+            let pt = {
+              x:center_pt.x+Math.sin(_bianAngle*Math.PI/180)*_bianchang,
+              y:center_pt.y+Math.cos(_bianAngle*Math.PI/180)*_bianchang
+            }
+
+            self.circlelist.push({
+              cx:pt.x,
+              cy:pt.y
+            })
+
+
+            // let dot = document.createElement('div');
+            // dot.style.position = 'absolute';
+            // dot.style.width = '5px';
+            // dot.style.height = '5px';
+            // dot.style.left =
+            let format = pt.x+','+pt.y
+            graphics_arr.push(format);
+        }
+        return graphics_arr.join(' ')
+      }
+    },
+    mounted:function(){
+
+    }
+  }
+
+</script>
+<style scoped="">
+  .container{
+    position: relative;
+    border:2px solid red;
+    width:10rem;
+    height:10rem;
+
+  }
+</style>
